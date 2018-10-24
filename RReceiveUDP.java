8f3cbe337c144073060d798de156bdb6f8118f78
Anush Shrestha <ashres13@asu.edu>
2018-10-24 02:16:18 -0500

    Send packet by sender

--- RReceiveUDP.java
+++ RReceiveUDP.java
@@ -1,18 +1,259 @@
+
+import java.awt.List;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.DatagramPacket;
-import edu.utulsa.unet.UDPSocket; //import java.net.DatagramSocket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.TreeMap;
+import java.util.Vector;
+import java.util.concurrent.Semaphore;
+import java.util.zip.CRC32;
+
+// Import from inet.java
+import edu.utulsa.unet.UDPSocket; //import java.net.DatagramSocket;
 import edu.utulsa.unet.RReceiveUDPI;
 
 public class RReceiveUDP implements RReceiveUDPI {
 
 	int MODE = 0; // 0 is stop and wait
-	long WINDOW_SIZE = 256;
+	long WINDOW_SIZE = 256; // bytes
 	long TIME_OUT = 1000; // in millisecond
-	String FILENAME;
+	String receiverFileName;
 	int localPort = 32456;
 
+	int prevSeqNum; // previous sequence number received in-order
+	int nextSeqNum; // next expected sequence number
+	boolean isTransferComplete;// (flag) if transfer is complete
+	int MTU = 20;
+	int pkt_size = MTU;
+	Semaphore s;
+	TreeMap<Integer, byte[]> packetsList;
+
+	Timer timer;
+
+	public RReceiveUDP() {
+		try {
+			System.out.println("Receiver: local IP: " + InetAddress.getLocalHost().getHostAddress() + " ARQ Algorithm: "
+					+ getModeName() + " ,local UDP port: " + getLocalPort());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) {
-		setMode(1);
+		RReceiveUDP receiver = new RReceiveUDP();
+		receiver.setMode(1);
+		receiver.setModeParameter(5);
+		receiver.setFilename("less_important.txt");
+		receiver.setLocalPort(32456);
+		receiver.receiveFile();
+	}
+
+	public int getLargestContinuousSequenceNumber(Vector<byte[]> packetsList) {
+		// Object minObj = Collections.min(packetsList.get(nextSeqNum));
+		return nextSeqNum;
+	}
+
+	public boolean receiveFile() {
+		try {
+			int prevSeqNum = -1; // previous sequence number received in-order
+			int nextSeqNum = 0; // next expected sequence number
+			boolean isTransferComplete = false; // (flag) if transfer is complete
+			s = new Semaphore(1);
+			packetsList = new TreeMap<Integer, byte[]>();
+
+			int inDatagramDestinationPort = getLocalPort();
+
+			int outDatagramDestinationPort = 23456;
+			DatagramSocket outDatagramSocket, inDatagramSocket;
+			// create sockets
+			outDatagramSocket = new DatagramSocket(); // outgoing channel
+			inDatagramSocket = new DatagramSocket(32456); // incoming channel
+
+			// create threads to process data
+			InConnectionThread th_in = new InConnectionThread(inDatagramSocket);
+			OutConnectionThread th_out = new OutConnectionThread(outDatagramSocket, outDatagramDestinationPort,
+					inDatagramDestinationPort);
+			th_in.start();
+			th_out.start();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		return true;
+	}
+
+	// check valid packet and build file
+	public class InConnectionThread extends Thread {
+		DatagramSocket inConnectionSocket, outConnectionSocket;
+		int sourcePort;
+		int destinationPort;
+
+		// InConnectionThread constructor
+		public InConnectionThread(DatagramSocket inConnectionSocket) {
+			this.inConnectionSocket = inConnectionSocket;
+		}
+
+		public void run() {
+			try {
+				FileOutputStream fos = new FileOutputStream(receiverFileName);
+				outConnectionSocket = new DatagramSocket();
+				System.out.println("Receiver: Listening ");
+				try {
+					byte[] in_data = new byte[pkt_size]; // message data in packet
+					DatagramPacket in_pkt = new DatagramPacket(in_data, in_data.length); // incoming packet
+					InetAddress dst_addr = InetAddress.getByName("172.17.34.56");
+					File file = new File(receiverFileName);
+
+					boolean isFinalSequeunceNum = false;
+					int windowSize = 0;
+					while (!isTransferComplete) {
+						if (packetsList.size() <= getWindowSize() && !isFinalSequeunceNum) {
+							s.acquire();
+							try {
+								System.out.println("1");
+								inConnectionSocket.receive(in_pkt);
+								System.out.println("2");
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+
+							byte[] received_checksum = copyOfRange(in_data, 0, 8);
+							CRC32 checksum = new CRC32();
+							checksum.update(copyOfRange(in_data, 8, in_pkt.getLength()));
+							byte[] calculated_checksum = ByteBuffer.allocate(8).putLong(checksum.getValue()).array();
+
+							// if packet is not corrupted
+							if (Arrays.equals(received_checksum, calculated_checksum)) {
+
+								byte[] inData = new byte[getWindowSize()];
+
+								int seqNum = ByteBuffer.wrap(copyOfRange(in_data, 8, 12)).getInt();
+								System.out.println("Receiver: Received sequence number: " + seqNum);
+
+								// in order or not in order store if in limit of window
+
+								byte[] payload = new byte[in_pkt.getLength() - 12];
+								payload = copyOfRange(in_data, 12, in_pkt.getLength() - 12);
+								packetsList.put(seqNum, payload);
+
+								// if final packet
+								if (in_pkt.getLength() == 12) {
+									// send ack
+									byte[] ackPkt = generatePacket(-2);
+									outConnectionSocket.send(new DatagramPacket(ackPkt, ackPkt.length, dst_addr, destinationPort));
+									isFinalSequeunceNum = true;
+									System.out.println("Receiver: All packets received!");
+								} else { // other packet
+
+									// in order = update pointer and remove
+									if (seqNum == nextSeqNum) {
+										int largestContinuousSeqNum = nextSeqNum;
+										// update pointer and create file to largest seq number
+
+										// for latest in order packet
+
+										fos.write(packetsList.get(nextSeqNum));
+										byte[] ackPkt = generatePacket(prevSeqNum);
+										outConnectionSocket.send(new DatagramPacket(ackPkt, ackPkt.length, dst_addr, destinationPort));
+										System.out.println("Receiver: Sent Ack for " + seqNum);
+										packetsList.remove(nextSeqNum);
+										prevSeqNum = seqNum; // previous received in order
+										nextSeqNum++; // next expected
+
+										if (packetsList.size() > 1) {
+											// for other pending acks
+											for (int i = nextSeqNum; i <= packetsList.lastKey(); i++) {
+												if (packetsList.containsKey(i)) {
+													fos.write(packetsList.get(nextSeqNum));
+													packetsList.remove(nextSeqNum);
+													prevSeqNum = seqNum; // previous received in order
+													nextSeqNum++; // next expected
+													largestContinuousSeqNum = i;
+												}
+											}
+											// send ack only for largest one
+											ackPkt = generatePacket(largestContinuousSeqNum);
+											outConnectionSocket.send(new DatagramPacket(ackPkt, ackPkt.length, dst_addr, destinationPort));
+											System.out.println("Receiver: Sent Ack for " + seqNum);
+										}
+
+									}
+								}
+							} // else packet is corrupted
+							else {
+								System.out.println("Receiver: Corrupt packet dropped.");
+								// byte[] ackPkt = generatePacket(prevSeqNum);
+								// outConnectionSocket.send(new DatagramPacket(ackPkt, ackPkt.length, dst_addr,
+								// destinationPort));
+								// System.out.println("Receiver: Sent duplicate Ack " + prevSeqNum);
+							}
+							s.release();
+						} else {
+							System.out.println("Receiver: Out of window. Packet dropped.");
+						}
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+					System.exit(-1);
+				} finally {
+					setTimer(false);
+					inConnectionSocket.close();
+					fos.close();
+					System.out.println("Receiver: inConnectionSocket closed!");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.exit(-1);
+			}
+		}
+
+	}
+
+	// send ack
+	public class OutConnectionThread extends Thread {
+
+		private DatagramSocket inConnectionSocket, outConnnectionSocket;
+		private int sourcePort;
+		private int destinationPort;
+
+		// constructor
+		public OutConnectionThread(DatagramSocket outConnectionSocket, int sourcePort, int destinationPort) {
+			this.outConnnectionSocket = outConnectionSocket;
+			this.destinationPort = destinationPort;
+			this.sourcePort = sourcePort;
+		}
+
+	}
+
+	// generate Ack packet
+	public byte[] generatePacket(int ackNum) {
+		byte[] ackNumBytes = ByteBuffer.allocate(4).putInt(ackNum).array();
+		// calculate checksum
+		CRC32 checksum = new CRC32();
+		checksum.update(ackNumBytes);
+		// construct Ack packet
+		ByteBuffer pktBuf = ByteBuffer.allocate(12);
+		pktBuf.put(ByteBuffer.allocate(8).putLong(checksum.getValue()).array());
+		pktBuf.put(ackNumBytes);
+		return pktBuf.array();
+	}
+
+	// same as Arrays.copyOfRange in 1.6
+	public byte[] copyOfRange(byte[] srcArr, int start, int end) {
+		int length = (end > srcArr.length) ? srcArr.length - start : end - start;
+		byte[] destArr = new byte[length];
+		System.arraycopy(srcArr, start, destArr, 0, length);
+		return destArr;
 	}
 
 	public boolean setMode(int mode) {
@@ -41,11 +282,11 @@ public class RReceiveUDP implements RReceiveUDPI {
 	}
 
 	public void setFilename(String fname) {
-		this.FILENAME = fname;
+		this.receiverFileName = fname;
 	}
 
 	public String getFilename() {
-		return this.FILENAME;
+		return this.receiverFileName;
 	}
 
 	public boolean setLocalPort(int port) {
@@ -57,24 +298,31 @@ public class RReceiveUDP implements RReceiveUDPI {
 		return this.localPort;
 	}
 
-	public boolean receiveFile() {
-		try {
-
-			System.out.println("Receiver: local IP=" + InetAddress.getLocalHost().getHostAddress() + "ARQ Algorithm:"
-					+ getModeName() + "local UDP port" + getLocalPort());
-
-			byte[] buffer = new byte[11];
-			UDPSocket socket = new UDPSocket(localPort);
-			DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-			socket.receive(packet);
-			InetAddress client = packet.getAddress();
-			System.out.println(" Received'" + new String(buffer) + "' from " + packet.getAddress().getHostAddress()
-					+ " with sender port " + packet.getPort());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return true;
+	public int getWindowSize() {
+		return (int) this.WINDOW_SIZE;
 	}
 
+	// Timeout task
+	public class Timeout extends TimerTask {
+		public void run() {
+			try {
+				s.acquire(); /***** enter CS *****/
+				System.out.println("Sender: Timeout!");
+				nextSeqNum = prevSeqNum; // resets nextSeqNum
+				s.release(); /***** leave CS *****/
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}// END CLASS Timeout
+
+	// to start or stop the timer
+	public void setTimer(boolean isNewTimer) {
+		if (timer != null)
+			timer.cancel();
+		if (isNewTimer) {
+			timer = new Timer();
+			timer.schedule(new Timeout(), TIME_OUT);
+		}
+	}
 }
--- RSendUDP.java
+++ RSendUDP.java
@@ -5,104 +5,386 @@
  * and open the template in the editor.
  */
 
+// java default
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.file.FileSystem;
+import java.util.Arrays;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Vector;
+import java.util.concurrent.Semaphore;
+import java.util.zip.CRC32;
+
+// from unet.jar file
 import edu.utulsa.unet.UDPSocket;
 import edu.utulsa.unet.RSendUDPI;
-import java.net.InetSocketAddress;
 
 /**
  *
  * @author ans7740
  */
 public class RSendUDP implements RSendUDPI {
-    static final String SERVER = "localhost";
+	static final String SERVER = "localhost";
 
-    int MODE = 0; // 0 is stop and wait
-    long WINDOW_SIZE = 256;
-    long TIME_OUT = 1000; // in millisecond
-    String FILENAME;
-    int localPort = 12987;
-    InetSocketAddress receiverInfo = new InetSocketAddress("localhost", this.localPort);
+	int MODE = 0; // 0 is stop and wait
+	long WINDOW_SIZE = 5;
+	long TIME_OUT = 1000; // in millisecond
+	String senderFileName;
+	int localPort = 12987;
+	InetSocketAddress receiverInfo;
+	int MTU = 20;
 
-    public static void main(String[] args) {
+	int baseSeqOfWindow;
+	int nextSeqInWindow;
+	Vector<byte[]> packetsList;
+	Timer timer;
+	Semaphore s;
+	boolean isTransferComplete;
 
-    }
+	// Timeout task
+	public class Timeout extends TimerTask {
+		public void run() {
+			try {
+				s.acquire(); /***** enter CS *****/
+				System.out.println("Sender: Timeout!");
+				nextSeqInWindow = baseSeqOfWindow; // resets nextSeqNum
+				s.release(); /***** leave CS *****/
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}// END CLASS Timeout
 
-    public boolean setMode(int mode) {
-        this.MODE = mode;
-        return true;
-    }
+	// to start or stop the timer
+	public void setTimer(boolean isNewTimer) {
+		if (timer != null)
+			timer.cancel();
+		if (isNewTimer) {
+			timer = new Timer();
+			timer.schedule(new Timeout(), TIME_OUT);
+		}
+	}
 
-    public int getMode() {
-        return this.MODE;
-    }
+	public static void main(String[] args) {
+		RSendUDP sender = new RSendUDP();
+		sender.setMode(1);
+		sender.setModeParameter(5);
+		sender.setTimeout(10000);
+		sender.setFilename("important.txt");
+		sender.setLocalPort(23456);
+		sender.setReceiver(new InetSocketAddress("172.17.34.56", 32456));
+		sender.sendFile();
+	}
 
-    public String getModeName() {
-        if (this.MODE == 0)
-            return "Stop and Wait";
-        else
-            return "Sliding Window";
-    }
+	public boolean sendFile() {
+		try {
+			String localIPAddress = InetAddress.getLocalHost().getHostAddress();
+			int localPort = getLocalPort();
 
-    public boolean setModeParameter(long n) {
-        this.WINDOW_SIZE = n;
-        return true;
-    }
+			String destinationIPAddress = getReceiver().getAddress().getHostAddress();
+			int destinationPort = getReceiver().getPort();
+			String file = getFilename();
+			System.out.println("Sending " + file + " from " + localIPAddress + ":" + localPort + " to " + destinationIPAddress
+					+ ":" + destinationPort + " with " + new File(file).length() + " bytes");
 
-    public long getModeParameter() {
-        return this.WINDOW_SIZE;
-    }
+			System.out.println("Sender: local IP: " + localIPAddress + " ARQ Algorithm: " + getModeName()
+					+ " local UDP port: " + getLocalPort());
 
-    public void setFilename(String fname) {
-        this.FILENAME = fname;
-    }
+			baseSeqOfWindow = 1;
+			nextSeqInWindow = 1;
+			// this.path = path;
+			// fileName = getFilename();
+			s = new Semaphore(1);
+			packetsList = new Vector<byte[]>(getWindowSize() - 1);
+			isTransferComplete = false;
 
-    public String getFilename() {
-        return this.FILENAME;
-    }
+			int inDatagramDestinationPort = getLocalPort();
+			int outDatagramDestinationPort = getReceiver().getPort();
+			DatagramSocket outDatagramSocket, inDatagramSocket;
+			// create sockets
+			outDatagramSocket = new DatagramSocket(); // outgoing channel
+			inDatagramSocket = new DatagramSocket(inDatagramDestinationPort); // incoming channel
+			String destinationInetAddress = getReceiver().getHostName();
 
-    public boolean setTimeout(long timeout) {
-        this.TIME_OUT = timeout;
-        return true;
-    }
+			// create threads to process data
+			InConnectionThread th_in = new InConnectionThread(inDatagramSocket);
+			OutConnectionThread th_out = new OutConnectionThread(outDatagramSocket, outDatagramDestinationPort,
+					destinationInetAddress);
+			th_in.start();
+			th_out.start();
 
-    public long getTimeout() {
-        return this.TIME_OUT;
-    }
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		return true;
+	}
 
-    public boolean setLocalPort(int port) {
-        this.localPort = port;
-        return true;
-    }
+	// create segment and send
+	public class OutConnectionThread extends Thread {
+		private DatagramSocket outConnnectionSocket;
+		private int destinationPort;
+		private String destinationInetAddress;
 
-    public int getLocalPort() {
-        return this.localPort;
-    }
+		// constructor
+		public OutConnectionThread(DatagramSocket outConnectionSocket, int destinationPort, String destinationInetAddress) {
+			this.outConnnectionSocket = outConnectionSocket;
+			this.destinationPort = destinationPort;
+			this.destinationInetAddress = destinationInetAddress;
+		}
 
-    public boolean setReceiver(InetSocketAddress receiver) {
-        this.receiverInfo = receiver;
-        return true;
-    }
+		// constructs the packet prepended with header information
+		public byte[] generatePacket(int sequenceNumber, byte[] dataBytes) {
+			byte[] sequenceNumberBytes = ByteBuffer.allocate(4).putInt(sequenceNumber).array();
 
-    public InetSocketAddress getReceiver() {
-        return this.receiverInfo;
-    }
+			// generate checksum
+			CRC32 checksum = new CRC32();
+			checksum.update(sequenceNumberBytes);
+			checksum.update(dataBytes);
+			byte[] checksumBytes = ByteBuffer.allocate(8).putLong(checksum.getValue()).array(); // checksum (8 bytes)
 
-    public boolean sendFile() {
-        try {
-            System.out.println("Sender: local IP=" + InetAddress.getByName(SERVER) + "ARQ Algorithm:" + getModeName()
-                    + "local UDP port" + getLocalPort());
+			// generate packet
+			ByteBuffer packeByteBuffer = ByteBuffer.allocate(8 + 4 + dataBytes.length);
+			packeByteBuffer.put(checksumBytes);
+			packeByteBuffer.put(sequenceNumberBytes);
+			packeByteBuffer.put(dataBytes);
+			return packeByteBuffer.array();
+		}
 
-            byte[] buffer = ("Hello World").getBytes();
-            UDPSocket socket = new UDPSocket();
+		public void run() {
+			try {
+				FileInputStream fileInputStream = new FileInputStream(senderFileName);
+				// int fileSizeInBytes = new File(senderFileName).length();
+				int counter = 1;
+				try {
+					boolean isFinalSequenceNum = false;
+					while (!isTransferComplete) {
+						// System.out
+						// .println(nextSeqInWindow + "," + baseSeqOfWindow + "," + getWindowSize() +
+						// "," + packetsList.size());
 
-            socket.send(new DatagramPacket(buffer, buffer.length, InetAddress.getByName(SERVER), localPort));
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println(e);
-        }
-        return true;
-    }
+						int headerLength = 12;
+						int payloadLength = MTU - headerLength;
+						int numberOfFrame = (int) new File(getFilename()).length() / payloadLength;
+						int sizeOfLastFrame = (int) new File(getFilename()).length() - (payloadLength * numberOfFrame);
 
+						if (nextSeqInWindow < baseSeqOfWindow + getWindowSize() - 1 && !isFinalSequenceNum) {
+							s.acquire();
+							if (baseSeqOfWindow == nextSeqInWindow) {
+								// first packet so start timer
+								setTimer(true);
+							}
+
+							byte[] outData = new byte[getWindowSize()];
+
+							// if in packetlist retrieve
+							if (nextSeqInWindow < packetsList.size()) {
+								outData = packetsList.get(nextSeqInWindow);
+							}
+
+							// else create and add to list
+							else {
+
+								byte[] dataBuffer = new byte[MTU];
+								int dataLength = fileInputStream.read(dataBuffer, 0, payloadLength);
+
+								// no more data to be read
+								if (dataLength == -1) {
+									isFinalSequenceNum = true;
+									outData = generatePacket(nextSeqInWindow, new byte[0]);
+								} else { // else if valid data
+									byte[] dataBytes = copyOfRange(dataBuffer, 0, dataLength);
+									outData = generatePacket(nextSeqInWindow, dataBytes);
+								}
+
+								// }
+								packetsList.add(outData);
+							}
+
+							// System.out.println(" pack list " + packetsList);
+							// sending the packet
+							outConnnectionSocket.send(new DatagramPacket(outData, outData.length,
+									InetAddress.getByName(destinationInetAddress), destinationPort));
+
+							int totalBytes = outData.length;
+							System.out.println(
+									"Sender : Message " + nextSeqInWindow + " sent with " + totalBytes + " bytes of actual data");
+;
+							if (!isFinalSequenceNum) {
+								nextSeqInWindow++;
+							}
+							s.release();
+						}
+						sleep(15);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					setTimer(false);
+					outConnnectionSocket.close();
+					fileInputStream.close();
+					System.out.println("Sender: Connection Socket Closed.");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.exit(-1);
+			}
+		}
+	}
+
+	// check for ack
+	public class InConnectionThread extends Thread {
+		private DatagramSocket inConnectionSocket;
+
+		// InConnectionThread constructor
+		public InConnectionThread(DatagramSocket inConnectionSocket) {
+			this.inConnectionSocket = inConnectionSocket;
+		}
+
+		// returns -1 if corrupted, else return Ack number
+		int decodePacket(byte[] packet) {
+			byte[] receivedCheckSumByte = copyOfRange(packet, 0, 8);
+			byte[] ackNumBytes = copyOfRange(packet, 8, 12);
+			CRC32 checksum = new CRC32();
+			checksum.update(ackNumBytes);
+			byte[] calculatedCheckSumByte = ByteBuffer.allocate(8).putLong(checksum.getValue()).array();// checksum (8 bytes)
+			if (Arrays.equals(receivedCheckSumByte, calculatedCheckSumByte))
+				return ByteBuffer.wrap(ackNumBytes).getInt();
+			else
+				return -1;
+		}
+
+		// receiving process (updates base)
+		public void run() {
+			try {
+				byte[] inData = new byte[12]; // ack packet with no data
+				DatagramPacket inPacket = new DatagramPacket(inData, inData.length);
+				try {
+					// while there are still packets yet to be received by receiver
+					while (!isTransferComplete) {
+
+						inConnectionSocket.receive(inPacket);
+						int ackNum = decodePacket(inData);
+						System.out.println("Sender: Message " + ackNum + "acknowledged");
+						// if ack is not corrupted
+						if (ackNum != -1) {
+							// if duplicate ack
+							// if (baseSeqOfWindow == ackNum + 1) {
+							// s.acquire(); /***** enter CS *****/
+							// setTimer(false); // off timer
+							// nextSeqInWindow = baseSeqOfWindow; // resets nextSeqInWindow
+							// s.release(); /***** leave CS *****/
+							// }
+							// else if teardown ack
+							if (ackNum == -2)
+								isTransferComplete = true;
+							// else normal ack
+							else {
+								baseSeqOfWindow = ackNum++; // update baseSeqOfWindow number
+								s.acquire(); /***** enter CS *****/
+								if (baseSeqOfWindow == nextSeqInWindow)
+									setTimer(false); // if no more unacknowledged packets in pipe, off timer
+								else
+									setTimer(true); // else packet acknowledged, restart timer
+								s.release(); /***** leave CS *****/
+							}
+						}
+						// else if ack corrupted, do nothing
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					inConnectionSocket.close();
+					System.out.println("Sender: inConnectionSocket closed!");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.exit(-1);
+			}
+		}
+	}// END CLASS InThread
+
+	// Constructor
+	public RSendUDP() {
+
+	}
+
+	public boolean setMode(int mode) {
+		this.MODE = mode;
+		return true;
+	}
+
+	public int getMode() {
+		return this.MODE;
+	}
+
+	public String getModeName() {
+		if (this.MODE == 0)
+			return "Stop and Wait";
+		else
+			return "Sliding Window";
+	}
+
+	public boolean setModeParameter(long n) {
+		this.WINDOW_SIZE = n;
+		return true;
+	}
+
+	public long getModeParameter() {
+		return this.WINDOW_SIZE;
+	}
+
+	public int getWindowSize() {
+		return (int) this.WINDOW_SIZE;
+	}
+
+	public void setFilename(String fname) {
+		this.senderFileName = fname;
+	}
+
+	public String getFilename() {
+		return this.senderFileName;
+	}
+
+	public boolean setTimeout(long timeout) {
+		this.TIME_OUT = timeout;
+		return true;
+	}
+
+	public long getTimeout() {
+		return this.TIME_OUT;
+	}
+
+	public boolean setLocalPort(int port) {
+		this.localPort = port;
+		return true;
+	}
+
+	public int getLocalPort() {
+		return this.localPort;
+	}
+
+	public boolean setReceiver(InetSocketAddress receiver) {
+		this.receiverInfo = receiver;
+		return true;
+	}
+
+	public InetSocketAddress getReceiver() {
+		return this.receiverInfo;
+	}
+
+	// same as Arrays.copyOfRange in 1.6
+	public byte[] copyOfRange(byte[] srcArr, int start, int end) {
+		int length = (end > srcArr.length) ? srcArr.length - start : end - start;
+		byte[] destArr = new byte[length];
+		System.arraycopy(srcArr, start, destArr, 0, length);
+		return destArr;
+	}
 }
